d1d89141d90744bf537c80a49e37930b10c4bfa4
sama85 <samayasser8520@gmail.com>
2023-03-15 16:17:35 +0200

    added logic of bottom navigation without navigation library

--- app/src/main/java/com/example/launchpadtaskslist/ui/TasksActivity.kt
+++ app/src/main/java/com/example/launchpadtaskslist/ui/TasksActivity.kt
@@ -2,11 +2,32 @@ package com.example.launchpadtaskslist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.launchpadtaskslist.R
+import com.example.launchpadtaskslist.databinding.ActivityMainBinding
 
 class TasksActivity: AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.tasksFragment -> loadFragment(TasksFragment())
+                R.id.accountFragment -> loadFragment(AccountFragment())
+                R.id.walletFragment -> loadFragment(WalletFragment())
+            }
+
+            true
+        }
+    }
+
+    fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment,fragment)
+        transaction.commit()
     }
 }
